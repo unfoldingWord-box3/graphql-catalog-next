73eb0853b0072edbbbeab3bb1860225afc0cce72
kavitharaju <kavitharaju18@gmail.com>
2021-06-04 18:36:16 +0530

    minimal working server

---
+++ server/schema/config.js
@@ -0,0 +1,9 @@
+const dbConnection = {
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'password',
+    database : 'dcs'
+ }
+
+module.exports.dbConnection = dbConnection
+ 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,13 @@
 const { ApolloServer } = require('apollo-server');
-const { typeDefs } = require('./schema/typedefs.js');
+const { typeDefs } = require('./schema/typedef.js');
 const { resolvers } = require('./schema/resolver.js');
 const { CatalogNext } = require('./schema/datasource.js')
+const { dbConnection } = require('./schema/config.js')
 
 // the database connetion details to be updated on deployment
 const knexConfig = {
   client: 'mysql',
-  connection: {
-    host : '127.0.0.1',
-    user : 'root',
-    password : 'password',
-    database : 'dcs'
-  }
+  connection: dbConnection
 };
 
 const db = new CatalogNext(knexConfig);
