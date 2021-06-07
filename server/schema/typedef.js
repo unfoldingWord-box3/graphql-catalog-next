e8dc69da96f0bc4c421f0ebeea0107b96211eb9f
Abel <abelper54@gmail.com>
2021-06-07 02:07:24 -0400

    added client and server into new branch

---
+++ server/schema/typedef.js
@@ -0,0 +1,127 @@
+const { gql } = require('apollo-server');
+
+let typeDefs = gql`
+	type ComputerLanguage {
+	   language: String
+	   isPrimary: Boolean
+	 }
+	 
+	 type Subjects {
+	   name: String
+	 }
+
+	type User {
+	   id: ID!
+	   login: String!
+	   name: String
+	   fullName: String!
+	   email: String!
+	   avatarUrl: String
+	   language: String
+	   isAdmin: Boolean
+	   lastLogin: String
+	   created: String
+	   repos: [Repo]
+	 }
+
+    type Repo {
+	   id: ID!
+	   owner: User!
+	   name: String!
+	   fullName: String!
+	   description: String!
+	   repoLanguages: [ComputerLanguage]
+	   repoSubjects: [Subjects]
+	   empty: Boolean
+	   private: Boolean
+	   fork: Boolean
+	   template: Boolean
+	   mirror: Boolean
+	   size: Int
+	   htmlUrl: String
+	   sshUrl: String
+	   cloneUrl: String
+	   originalUrl: String
+	   website: String
+	   starsCount: Int
+	   forksCount: Int
+	   watchersCount: Int
+	   openIssues_count: Int
+	   openPrCounter: Int
+	   releaseCounter: Int
+	   defaultBranch: String
+	   archived: Boolean
+	   createdAt: String
+	   updatedAt: String
+	   #permissions: Permissions
+	   hasIssues: Boolean,
+	   avatarUrl: String
+	   language: String
+	   subject: String
+	   books: String
+	   title: String
+	   checkingLevel: String
+	 }
+
+	type Organization {
+	   id: ID!
+	   login: String!
+	   name: String!
+	   website: String
+	   location: String
+	   email: String
+	   avatarUrl: String
+	   language: String
+	   isAdmin: Boolean
+	   lastLogin: String
+	   created: String
+	   teams: [Team]
+	   members: [User]
+	 }
+
+	 type Team {
+	 	id: ID!
+	 	name: String
+	 	description: String
+	 	organization: Organization
+	 	teamRepos: [Repo]
+	 	teamType: String
+	 	members: [User]
+
+	 }
+
+	 scalar TypelessData
+
+	 type Catalog {
+	 	metadataVersion: String
+	 	metadata: TypelessData!
+	 	repo: Repo!
+	 	release: Release
+	 	stage: Int
+	 	branchOrTag: String!
+	 	releaseDateUnix: String
+	 }
+
+	 type Release {
+	 	title: String
+	 	target: String
+	 	note: String
+	 	publisher: User
+	 	originalAuthor: User
+	 }
+
+	type Query {
+		allOrgs: [Organization]
+		orgsByName: [Organization]
+		user(name: String): User
+		userSearch(name: String): [User]
+		repo(repoName: String, userName: String): Repo
+		reposByName(name: String): [Repo]
+		catalog(repoName: String, userName: String, branchOrTag: String): Catalog
+		catalogsByRepo(repoName: String): [Catalog]
+		catalogsByOwner(userName: String): [Catalog]
+		catalogSearch(searchWord: String): [Catalog]
+	}
+`;
+
+module.exports.typeDefs = typeDefs
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require('apollo-server');
+const { typeDefs } = require('./schema/typedef.js');
+const { resolvers } = require('./schema/resolver.js');
+const { CatalogNext } = require('./schema/datasource.js')
+const { dbConnection } = require('./schema/config.js')
+
+// the database connetion details to be updated on deployment
+const knexConfig = {
+  client: 'mysql',
+  connection: dbConnection
+};
+
+const db = new CatalogNext(knexConfig);
+
+
+const server = new ApolloServer({ typeDefs, resolvers,  dataSources: () => ({catalogNext: db})});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
